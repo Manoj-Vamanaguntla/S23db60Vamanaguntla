a5ed5a42ab403d7e88cf14a5884f2e03e3be49b1
S560460 <S560460@nwmissouri.edu>
2023-04-19 23:31:29 -0500

    done

--- controllers/van.js
+++ controllers/van.js
@@ -14,10 +14,18 @@ exports.van_list = async function(req, res) {
     }
    };
 
-// for a specific van.
-exports.van_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: van detail: ' + req.params.id);
-};
+
+// for a specific Van.
+exports.van_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await van.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 // Handle van create on POST.
 
  //res.send('NOT IMPLEMENTED: van create POST');
@@ -42,12 +50,38 @@ exports.van_create_post = async function(req, res) {
     }
 };
 // Handle van delete form on DELETE.
-exports.van_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: van delete DELETE ' + req.params.id);
-};
-// Handle van update form on PUT.
-exports.van_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: van update PUT' + req.params.id);
+// Handle van delete on DELETE.
+exports.van_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await van.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
+// Handle Van update form on PUT.
+exports.van_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await van.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.van_color)
+ toUpdate.van_color = req.body.van_color;
+ if(req.body.van_size) toUpdate.van_size = req.body.van_size;
+ if(req.body.van_cost) toUpdate.van_cost = req.body.van_cost;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
 
 
@@ -61,5 +95,58 @@ exports.van_view_all_Page = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
+    }
+   };
+
+   // Handle a show one view with id specified by query
+exports.van_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await van.findById( req.query.id)
+    res.render('vandetail',
+   { title: 'van Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle building the view for updating a van.
+// query provides the id
+exports.van_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await van.findById(req.query.id)
+    res.render('vanupdate', { title: 'van Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle building the view for creating a van.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.van_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('vancreate', { title: 'van Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle a delete one view with id from query
+exports.van_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await van.findById(req.query.id)
+    res.render('vandelete', { title: 'van Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
--- routes/van.js
+++ routes/van.js
@@ -3,4 +3,13 @@ const van_controlers= require('../controllers/van');
 var router = express.Router();
 /* GET vans */
 router.get('/', van_controlers.van_view_all_Page );
+/* GET detail van page */
+router.get('/detail', van_controlers.van_view_one_Page);
+/* GET create update page */
+router.get('/update', van_controlers.van_update_Page);
+/* GET create van page */
+router.get('/create', van_controlers.van_create_Page);
+/* GET delete van page */
+router.get('/delete', van_controlers.van_delete_Page);
 module.exports = router;
+
