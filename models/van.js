9d7a583df1ad37780df5e01de093fe0e4b01a5d1
S560460 <S560460@nwmissouri.edu>
2023-04-24 14:43:50 -0500

    done
    updates done

--- models/van.js
+++ models/van.js
@@ -1,8 +1,24 @@
 const mongoose = require("mongoose")
 const vanSchema = mongoose.Schema({
-    van_color: String,
-    van_size: String,
-    van_cost: Number
-})
+    // van_color: String,
+    // van_size: String,
+    // van_cost: Number
+    
+// New code added for Assignment13
+    van_color: {
+        type: String,
+        required: true
+    },
+    van_size: {
+        type: String,
+        required: true
+    },
+    van_cost: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10000
+    }
+});
 module.exports = mongoose.model("van",
 vanSchema)
--- routes/van.js
+++ routes/van.js
@@ -1,15 +1,29 @@
 var express = require('express');
 const van_controlers= require('../controllers/van');
 var router = express.Router();
+
+//New code added for Assignment13
+
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+
 /* GET vans */
 router.get('/', van_controlers.van_view_all_Page );
 /* GET detail van page */
-router.get('/detail', van_controlers.van_view_one_Page);
+router.get('/detail',secured, van_controlers.van_view_one_Page);
 /* GET create update page */
-router.get('/update', van_controlers.van_update_Page);
+router.get('/update',secured, van_controlers.van_update_Page);
 /* GET create van page */
-router.get('/create', van_controlers.van_create_Page);
+router.get('/create',secured, van_controlers.van_create_Page);
 /* GET delete van page */
-router.get('/delete', van_controlers.van_delete_Page);
+router.get('/delete',secured, van_controlers.van_delete_Page);
 module.exports = router;
 
